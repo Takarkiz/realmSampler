1882920f8bc343ada933c8c19469f72103f72fb5
Takarkiz <ponpontanuki16@gmail.com>
2017-11-30 16:03:46 +0900

    Initial Commit

---
+++ realmProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realmProject
+//
+//  Created by 澤田昂明 on 2017/11/30.
+//  Copyright © 2017年 澤田昂明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
